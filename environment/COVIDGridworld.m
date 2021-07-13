7038d8fba4054d1be981c056405b0b42eb5bc7c8
Roberto Masocco <robmasocco@gmail.com>
2021-07-13 13:30:23 +0200

    Added general properties.

--- environment/COVIDGridworld.m
+++ environment/COVIDGridworld.m
@@ -5,27 +5,43 @@
 % July 13, 2021
 
 classdef COVIDGridworld < rl.env.MATLABEnvironment
-    %COVIDGRIDWORLD: Multiple people RL gridworld.    
-    
+    %COVIDGRIDWORLD: Multiple people RL gridworld.
+
     %% Gridworld Properties
     properties
         % Environment constants and physical characteristics.
+        % People in the map.
+        n_people = 0
+        
+        % Matrix encoding the map state in real time.
+        map_mat = []
+        
+        % Stall-leading actions counter.
+        stall_acts_cnt = 0
+        
+        % "Defeat" state reward (depends on map size).
+        defeat_rew = 0
+        
+        % Single step reward.
+        single_step_rew = -1
+        
+        % Victory state reward.
+        victory_rew = 0
     end
-    
+
     properties
-        % System state, initialized to a vector (will be filled below).
-        State = zeros(4,1)
+        % People positions, initialized to a vector (depends on n_people).
+        State = []
     end
-    
+
     properties (Access = protected)
         % Internal flag to indicate episode termination.
-        IsDone = false        
+        IsDone = false
     end
 
     %% Necessary Methods
-    methods              
-        % Contructor method creates an instance of the environment
-        % Change class name and constructor name accordingly
+    methods
+        % Creates an instance of the environment.
         function this = COVIDGridworld()
             % Initialize Observation settings
             ObservationInfo = rlNumericSpec([4 1]);
@@ -37,21 +53,18 @@ classdef COVIDGridworld < rl.env.MATLABEnvironment
             ActionInfo.Name = 'CartPole Action';
             
             % The following line implements built-in functions of RL env
-            this = this@rl.env.MATLABEnvironment(ObservationInfo,ActionInfo);
-            
-            % Initialize property values and pre-compute necessary values
-            updateActionInfo(this);
+            this = this@rl.env.MATLABEnvironment(ObservationInfo, ActionInfo);
         end
-        
+
         % Apply system dynamics and simulates the environment with the 
         % given action for one step.
-        function [Observation,Reward,IsDone,LoggedSignals] = step(this,Action)
+        function [Observation,Reward,IsDone,LoggedSignals] = step(this, Action)
             
             % (optional) use notifyEnvUpdated to signal that the 
             % environment has been updated (e.g. to update visualization)
             notifyEnvUpdated(this);
         end
-        
+
         % Reset environment to initial state and output initial observation
         function InitialObservation = reset(this)
             
@@ -60,6 +73,7 @@ classdef COVIDGridworld < rl.env.MATLABEnvironment
             notifyEnvUpdated(this);
         end
     end
+
     %% Optional Methods
     methods               
         % Helper methods to create the environment
@@ -69,10 +83,6 @@ classdef COVIDGridworld < rl.env.MATLABEnvironment
                 error('Action must be %g for going left and %g for going right.',-this.MaxForce,this.MaxForce);
             end
             force = action;           
-        end
-        % update the action info based on max force
-        function updateActionInfo(this)
-            this.ActionInfo.Elements = this.MaxForce*[-1 1];
         end
         
         % Reward function
@@ -84,11 +94,11 @@ classdef COVIDGridworld < rl.env.MATLABEnvironment
             end          
         end
         
-        % (optional) Visualization method
+        % Visualization method.
         function plot(this)
-            % Initiate the visualization
+            % Initiate the visualization.
             
-            % Update the visualization
+            % Update the visualization.
             envUpdatedCallback(this)
         end
     end
